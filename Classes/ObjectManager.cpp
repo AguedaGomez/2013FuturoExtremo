d8cebcf7f63fc3e7b146f51bdc32b6ab4300712e
Javack <javatoons@gmail.com>
2015-12-20 21:48:11 +0100

    coloca todos los objetos interactuables y activa sus circulos de colision

--- Classes/ObjectManager.cpp
+++ Classes/ObjectManager.cpp
@@ -30,67 +30,67 @@ void ObjectManager::loadLevelObjects(int nivel)
 	switch (nivel) {
 	case 0:
 		Sprite *colaMachine = Sprite::createWithSpriteFrameName("cocacola.png");
-		colaMachine->setPosition(400,400);
+		colaMachine->setPosition(-480,166);
 		objects.insert(0, colaMachine);
 
 		Sprite *expMachine = Sprite::createWithSpriteFrameName("expendedora.png");
-		expMachine->setPosition(0, 400);
+		expMachine->setPosition(-620, 166);
 		objects.insert(1, expMachine);
 
 		Sprite *conducto = Sprite::createWithSpriteFrameName("conducto.png");
-		conducto->setPosition(0, 400);
+		conducto->setPosition(-1065, 309);
 		objects.insert(2, conducto);
 
 		Sprite *rejilla = Sprite::createWithSpriteFrameName("rejilla.png");
-		rejilla->setPosition(0, 400);
+		rejilla->setPosition(-1065, 309);
 		objects.insert(3, rejilla);
 
 		Sprite *puertaMadera1 = Sprite::createWithSpriteFrameName("puertaFrente1.png"); //planta 0 escaleras
-		puertaMadera1->setPosition(0, 400);
+		puertaMadera1->setPosition(-1356, 171);
 		objects.insert(4, puertaMadera1);
 
 		Sprite *puertaMadera2 = Sprite::createWithSpriteFrameName("puertaFrente2.png"); //planta 1 escaleras
-		puertaMadera2->setPosition(0, 400);
+		puertaMadera2->setPosition(-1359, 893);
 		objects.insert(5, puertaMadera2);
 
 		Sprite *puertaCanto1 = Sprite::createWithSpriteFrameName("puertaCanto1.png"); //planta 0 derecha
-		puertaCanto1->setPosition(0, 400);
+		puertaCanto1->setPosition(-76, 171);
 		objects.insert(6, puertaCanto1);
 
 		Sprite *puertaCanto2 = Sprite::createWithSpriteFrameName("puertaCanto2.png"); //planta 0 medio
-		puertaCanto2->setPosition(0, 400);
+		puertaCanto2->setPosition(-1604, 171);
 		objects.insert(7, puertaCanto2);
 
 		Sprite *puertaCanto3 = Sprite::createWithSpriteFrameName("puertaCanto3.png"); //planta 0 izq.
-		puertaCanto3->setPosition(0, 400);
+		puertaCanto3->setPosition(-3070, 171);
 		objects.insert(8, puertaCanto3);
 
 		Sprite *puertaCanto4 = Sprite::createWithSpriteFrameName("puertaCanto4.png"); //sotano
-		puertaCanto4->setPosition(0, 400);
+		puertaCanto4->setPosition(-1356, -549);
 		objects.insert(9, puertaCanto4);
 
 		Sprite *puertaCanto5 = Sprite::createWithSpriteFrameName("puertaCanto5.png"); //planta 1 derecha
-		puertaCanto5->setPosition(0, 400);
+		puertaCanto5->setPosition(-854, 891);
 		objects.insert(10, puertaCanto5);
 
 		Sprite *puertaCanto6 = Sprite::createWithSpriteFrameName("puertaCanto6.png");//planta 1 izq.
-		puertaCanto6->setPosition(0, 400);
+		puertaCanto6->setPosition(-1405, 891);
 		objects.insert(11, puertaCanto6);
 
 		Sprite *ascensor = Sprite::createWithSpriteFrameName("ascensor.png");
-		ascensor->setPosition(0, 400);
+		ascensor->setPosition(233, 183);
 		objects.insert(12, ascensor);
 
 		Sprite *puertaMetal1 = Sprite::createWithSpriteFrameName("puertaSotano1.png");//planta 0
-		puertaMetal1->setPosition(0, 400);
+		puertaMetal1->setPosition(-3406, 179);
 		objects.insert(13, puertaMetal1);
 
 		Sprite *puertaMetal2 = Sprite::createWithSpriteFrameName("puertaSotano2.png"); //sotano
-		puertaMetal2->setPosition(0, 400);
+		puertaMetal2->setPosition(-3406, -540);
 		objects.insert(14, puertaMetal2);
 
 		Sprite *conductoAbierto = Sprite::createWithSpriteFrameName("conductoConRejilla.png");
-		conductoAbierto->setPosition(0, 400);
+		conductoAbierto->setPosition(-1087, -472);
 		objects.insert(15, conductoAbierto);
 
 		break;
